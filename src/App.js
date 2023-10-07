efdd2a067d5df88472b620d0d8b8df7b2beea47c
Desire Oluwarotimi <rotimidesire@gmail.com>
2023-10-07 20:31:08 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [firstCurr, setFirstCurr] = useState([]);
+  const [firstSelect, setFirstSelect] = useState("USD");
+  const [secondCurr, setSecondCurr] = useState([]);
+  const [secondSelect, setSecondSelect] = useState("EUR");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  useEffect(() => {
+    async function fetchCurrencies() {
+      try {
+        const res = await fetch(`https://api.frankfurter.app/currencies`);
+        const data = await res.json();
+        setFirstCurr(Object.keys(data));
+        setSecondCurr(Object.keys(data));
+      } catch (error) {
+        console.error("Error fetching currencies:", error);
+      }
+    }
+
+    fetchCurrencies();
+  }, []);
+
+  const handleConvert = async () => {
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${firstSelect}&to=${secondSelect}`
+      );
+      const data = await res.json();
+      setConvertedAmount(data.rates[secondSelect]);
+    } catch (error) {
+      console.error("Error converting currencies:", error);
+    }
+  };
+
+  function handleAmount(e) {
+    setConvertedAmount(0);
+    setAmount(Number(e.target.value));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="top">
+        <img src="./cc.png" alt="cc" />
+        <h1>Currency Converter</h1>
+      </div>
+      <form>
+        <label htmlFor="amount">Amount</label>
+        <input type="text" id="amount" value={amount} onChange={handleAmount} />
+        <div className="select">
+          <select
+            value={firstSelect}
+            onChange={(e) => setFirstSelect(e.target.value)}
+          >
+            {firstCurr.map((curr) => (
+              <option value={curr} key={curr}>
+                {curr}
+              </option>
+            ))}
+          </select>
+          <select
+            value={secondSelect}
+            onChange={(e) => setSecondSelect(e.target.value)}
+          >
+            {secondCurr.map((curr) => (
+              <option value={curr} key={curr}>
+                {curr}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="button" onClick={handleConvert}>
+          Convert
+        </button>
+      </form>
+      <p>{`${amount} ${firstSelect} = ${convertedAmount} ${secondSelect}`}</p>
+    </main>
   );
 }
 
